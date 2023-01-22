222f62d60f9a7a0c019c7057146f0be936525c7e
Mayank Jha <raijin@cuphead.local>
2023-01-22 11:07:26 +0530

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm, NgModel} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultOption = 'pet';
+  answer = '';
+  genders = ['Male', 'Female']
+  @ViewChild('f') formElement: NgForm;
+  @ViewChild('email') emailElement: NgModel;
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  }
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.formElement.form.patchValue({
+      userData: {
+        username: suggestedName,
+      }
+    })
+  }
+
+  // onSubmit(form: NgForm){
+  //   console.dir(form);
+  // }
+  onSubmit() {
+    console.log(this.emailElement);
+    console.log(this.formElement);
+    this.submitted = true;
+    this.user.username = this.formElement.value.userData.username;
+    this.user.email = this.formElement.value.userData.email;
+    this.user.secretQuestion = this.formElement.value.secret;
+    this.user.answer = this.formElement.value.answer;
+    this.user.gender = this.formElement.value.gender;
   }
 }
